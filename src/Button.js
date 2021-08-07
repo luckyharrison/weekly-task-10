6b1ee08fdd20880d5c92e16b18617dadfe5c7098
Harrison Lucky <85343061+luckyharrison@users.noreply.github.com>
2021-08-07 04:51:38 -0700

    Delete src directory

--- src/Button.js
+++
@@ -1,3 +0,0 @@
-export default function Button() {
-    return <button>More <img src='../public/caret-right-solid.svg'></img></button>
-}
--- src/Button2.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default class Button2 extends React.Component {
-    render() {
-        return <button>Sign Out</button>
-    }
-
-}
